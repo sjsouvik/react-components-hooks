3189d2429a7e928f48f9974d7375121d550eaf1e
Souvik Jana <souvikjana2013@gmail.com>
2023-04-16 18:20:02 +0530

    refactor: update implementation of accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import "./Accordion.css";
 
-const Accordion = (props) => {
-  const { question, answer, id, selectedFaq, setSelectedFaq } = props;
+export const AccordionItem = (props) => {
+  const { label, active, handleClick, children } = props;
 
   return (
-    <div className={`accordion mt-1 ${selectedFaq === id ? "active" : ""}`}>
-      <section className="question" onClick={() => setSelectedFaq(id)}>
-        {question}
-      </section>
-      <section className="answer">{answer}</section>
+    <div className={`accordion mt-1 ${active ? "active" : ""}`}>
+      <button className="question" onClick={handleClick}>
+        {label}
+      </button>
+      <div className="answer">{children}</div>
     </div>
+  );
+};
+
+const Accordion = (props) => {
+  const { defaultActiveId, children } = props;
+  const [activeItemId, setActiveItemId] = useState(defaultActiveId);
+
+  const updateActiveItem = (itemId) => {
+    if (itemId === activeItemId) {
+      setActiveItemId("");
+      return;
+    }
+
+    setActiveItemId(itemId);
+  };
+
+  const accordionItems = children.filter(
+    (item) => item.type.name === "AccordionItem"
+  );
+
+  return (
+    <>
+      {accordionItems.map(({ props }) => (
+        <AccordionItem
+          key={props.id}
+          active={props.id === activeItemId}
+          handleClick={() => updateActiveItem(props.id)}
+          {...props}
+        />
+      ))}
+    </>
   );
 };
 
--- src/components/Accordion/Faqs.jsx
+++ src/components/Accordion/Faqs.jsx
@@ -1,38 +1,34 @@
-import { useState } from "react";
-import Accordion from "./Accordion";
+import Accordion, { AccordionItem } from "./Accordion";
 
-const data = [
+const accordionData = [
   {
-    id: 1,
+    id: "1",
     question: "Q. What is the national flower of India?",
     answer: "ans: Lotus",
   },
   {
-    id: 2,
+    id: "2",
     question: "Q. What is the national animal of India?",
     answer: "ans: Tiger",
   },
   {
-    id: 3,
+    id: "3",
     question: "Q. What is the national bird of India?",
     answer: "ans: Peacock",
   },
 ];
 
 export const Faqs = () => {
-  const [selectedFaq, setSelectedFaq] = useState(1);
-
   return (
     <div className="faqs">
       <h2>FAQs</h2>
-      {data.map((item) => (
-        <Accordion
-          key={item.id}
-          {...item}
-          selectedFaq={selectedFaq}
-          setSelectedFaq={setSelectedFaq}
-        />
-      ))}
+      <Accordion defaultActiveId="1">
+        {accordionData.map(({ id, question, answer }) => (
+          <AccordionItem key={id} id={id} label={question}>
+            {answer}
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 };
