686cf01601abecd9142c41de9ebbece613cb9fa1
Souvik Jana <souvikjana2013@gmail.com>
2023-04-22 18:02:19 +0530

    feat: add useToggle custom hook

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+/*
+Basically, what this hook does is that, it takes a parameter with value true or false and toggles that value to opposite. 
+It's useful when we want to take some action into it's opposite action, 
+
+for example: show and hide modal, show more/show less text, open/close side menu.
+*/
+
+export const useToggle = (initiaState = false) => {
+  const [toggle, setToggle] = useState(initiaState);
+
+  const updateToggle = () => setToggle((toggle) => !toggle);
+
+  return [toggle, updateToggle];
+};
