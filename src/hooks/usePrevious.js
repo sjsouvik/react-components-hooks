df677d303c7cdb5e019402dd59e01b856b4c1357
Souvik Jana <souvikjana2013@gmail.com>
2023-04-23 14:04:52 +0530

    docs: add the problem statement for usePrevious

--- src/hooks/usePrevious.js
+++ src/hooks/usePrevious.js
@@ -1,3 +1,16 @@
+/*
+
+One question that comes up a lot is "When using hooks how do I get the previous value of props or state?". 
+With React class components you have the componentDidUpdate method which receives previous props and state 
+as arguments or you can update an instance variable (this.previous = value) and reference it later to get 
+the previous value. So how can we do this inside a functional component that doesn't have lifecycle methods 
+or an instance to store values on? Hooks to the rescue! We can create a custom hook that uses the useRef hook 
+internally for storing the previous value.
+
+Create a hook usePrevious() to return the previous value, with initial value of `undefined`.
+ 
+*/
+
 import { useEffect } from "react";
 
 export const usePrevious = (value) => {
