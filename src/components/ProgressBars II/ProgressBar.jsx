cc3b2336f031551eb81a198d228e1f235642acc0
Souvik Jana <souvikjana2013@gmail.com>
2023-06-24 23:20:29 +0530

    docs: add problem statement

--- src/components/ProgressBars II/ProgressBar.jsx
+++ src/components/ProgressBars II/ProgressBar.jsx
@@ -1,3 +1,15 @@
+/*
+
+Build an app where clicking the "Add" button adds progress bars to the page. The progress bars fill up in series, aka the second bar only starts filling up after the first bar is completely filled.
+
+Requirements:
+
+Clicking on the "Add" button adds a progress bar to the page.
+The progress bars fill up gradually in sequence, one at a time. i.e. the second progress bar will only starts filling up after the first progress bar is completely filled up.
+Each bar takes approximately 2000ms to completely fill up.
+
+*/
+
 import { useEffect, useState } from "react";
 import "../ProgressBars/ProgressBar.css";
 
--- src/components/ProgressBars/ProgressBar.jsx
+++ src/components/ProgressBars/ProgressBar.jsx
@@ -1,3 +1,15 @@
+/*
+
+Build an app where clicking the "Add" button adds progress bars to the page. The progress bars fill up gradually as soon as they are shown.
+
+Requirements:
+
+Clicking on the "Add" button adds a progress bar to the page.
+Each progress bar start filling up smoothly as soon as they're added
+Each bar takes approximately 2000ms to completely fill up.
+
+*/
+
 import { useEffect, useState } from "react";
 import "./ProgressBar.css";
 
